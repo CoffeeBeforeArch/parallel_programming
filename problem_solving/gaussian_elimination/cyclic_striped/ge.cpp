66d97d82d4303343175ee997472c502c012a7752
Nick <CoffeeBeforeArch@gmail.com>
2020-06-19 14:36:14 -0500

    remove for now

--- problem_solving/gaussian_elimination/cyclic_striped/ge.cpp
+++
@@ -1,4 +0,0 @@
-// This program implements a broadcast parallel version of gaussian elimination
-// with a cyclic striped data mapping in C++
-// By: Nick from CoffeeBeforeArch
-
--- problem_solving/gaussian_elimination/pipelined/ge.cpp
+++
@@ -1,4 +0,0 @@
-// This program implements a pipelined parallel version of gaussian elimination
-// in C++
-// By: Nick from CoffeeBeforeArch
-
